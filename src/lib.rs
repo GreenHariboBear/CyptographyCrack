d9546608c3dd48cfa67f9bc21372c7eeda04d0cd
Patrick Lubovech <141656645+GreenHariboBear@users.noreply.github.com>
2023-08-15 19:16:30 +0300

    src added/gitignoe

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+pub mod set_1_basics;
+pub mod set_2_block_crypto;
+pub mod set_5_diffie_hellman_and_friends;
+pub mod utils;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
---
+++ src/set_1_basics.rs
@@ -0,0 +1,8 @@
+pub mod c1_convert_hex_to_base64;
+pub mod c2_fixed_xor;
+pub mod c3_single_byte_xor_cypher;
+pub mod c4_detect_single_char_xor;
+pub mod c5_repeating_key_xor;
+pub mod c6_break_repeating_key_xor;
+pub mod c7_aes_in_ecb_mode;
+pub mod c8_detect_aes_in_ecb_mode;
